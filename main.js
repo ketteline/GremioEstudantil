2db6df20b63581a9ea553efdd2a3a9a8eed75b0d
ketteline <137803631+ketteline@users.noreply.github.com>
2023-11-13 11:44:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-document.querySelector('#som_tecla_aplausos').play()
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
 
-document.querySelector(' .tecla_aplausos')
+document.querySelector('.tecla_aplausos').onclick = tocarSomAplausos;
